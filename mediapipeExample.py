81e9633c5ee161a721f741b506311b85e18f4586
Raphael Pattie <rpattiecsi@gmail.com>
2021-11-15 13:50:33 +0000

    glove example

--- mediapipeExample.py
+++ mediapipeExample.py
@@ -1,32 +1,61 @@
 import cv2
 import mediapipe as mp
 import time
+import serial
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 PRECISION = 0.1
 TIME_INTERVAL = 500000000
-TARGET_OUTLINE = False
+TARGET_OUTLINE = True
 
-def guide(position, imageHeight, imageWidth, marginOfError):
+def open_glove():
+    return serial.Serial('COM10')
+
+def close_glove(ser):
+    ser.close()
+
+
+def buzz(ser, loc, lenms=500):
+    '''Buzz given location for given duration in ms
+    location codes:
+    t, b, l, r, a'''
+    loc_ar = {'t':'1000', 'r':'0100', 'b':'0010', 'l':'0001', 'a':'1111'}
+    loc_code = loc_ar[loc]
+    com = loc_code+'buzz'+str(lenms)+'.'
+    ser.write(str.encode(com))
+
+def spin(times):
+    for i in range(times):
+        buzz('t',50)
+        buzz('r',50)
+        buzz('b',50)
+        buzz('l',50)
+
+def guide(position, imageHeight, imageWidth, marginOfError, ser):
     """Will print guiding information to guide the user's hand to the middle of the screen
     position: tupple with the hand's location
     imageHeight: height of the image being processed
     imageWidth: width of the image being processed
     marginOfError: double representing how far from the middle point 
+    ser: connection to the glove
     """
     halfX = imageWidth / 2
     halfY = imageHeight / 2
     # Check the x coordinate to guide to middle
     if position[0] > halfX * (1.0 - marginOfError) and not position[0] < halfX * (1.0 + marginOfError):
-        print("Position: {} Target: {} Left".format(position[0], halfX))
+        print("Left")
+        buzz(ser, "l")
     elif position[0] < halfX * (1.0 + marginOfError) and not position[0] > halfX * (1.0 - marginOfError):
         print("Right")
+        buzz(ser, "r")
         # Check the y coordinate to guide to middle
     if position[1] > halfY * (1.0 - marginOfError) and not position[1] < halfY * (1.0 + marginOfError):
         print("Up")
+        buzz(ser, "t")
     elif position[1] < halfY * (1.0 + marginOfError) and not position[1] > halfY * (1.0 - marginOfError):
         print("Down")
+        buzz(ser, "b")
 
 def drawTarget(image, imageHeight, imageWidth, marginOfError):
     """Roughly draws an outline of the target area
@@ -47,6 +76,9 @@ def drawTarget(image, imageHeight, imageWidth, marginOfError):
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
+# Open connection to glove
+ser = open_glove()
+
 with mp_hands.Hands(
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as hands:
@@ -87,7 +119,7 @@ with mp_hands.Hands(
                             # If there are coordinates for the wrist and time has elapsed since the last p
                             if pixelCoordinatesLandmark != None and (time.time_ns() - lastPrint) > TIME_INTERVAL:
                                 guide(pixelCoordinatesLandmark,
-                                    imageHeight, imageWidth, PRECISION)
+                                    imageHeight, imageWidth, PRECISION, ser)
                                 lastPrint = time.time_ns()
 
                     # Original Google example, draw hand annotations on the image
@@ -103,3 +135,4 @@ with mp_hands.Hands(
         if cv2.waitKey(5) & 0xFF == ord('q'):
             break
 cap.release()
+close_glove(ser)
